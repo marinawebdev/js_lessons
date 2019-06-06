9f0403af03a400238ba67e63f29e88553e859447
Marina <marina.vyskirka@gmail.com>
2019-06-06 14:22:12 +0300

    Upload script.js

--- task1/script.js
+++ task1/script.js
@@ -21,10 +21,40 @@ for ( let i = 0; i < 2; i++ ) {
 		console.log('done');
 		appData.expenses[a] = b;
 	} else {
-		a = prompt("Введите обязательную статью расходов в этом месяце", ""),
-		b = prompt("Во сколько обойдется?", "");
+		console.log('bad result');
+		i--;
 	};
 };
+
+/*while ( i < 2 ) {
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+		b = prompt("Во сколько обойдется?", "");
+
+	if ( (typeof(a)) === "string" && (typeof(a)) != null 
+		&& (typeof(b)) != null && a != "" && b != "" && a.length < 50 ) {
+		console.log('done');
+		appData.expenses[a] = b;
+		i++;
+	} else {
+		console.log('bad result');
+		i--;
+	};
+};
+
+do {
+	let a = prompt("Введите обязательную статью расходов в этом месяце", ""),
+		b = prompt("Во сколько обойдется?", "");
+		
+	if ( (typeof(a)) === "string" && (typeof(a)) != null 
+		&& (typeof(b)) != null && a != "" && b != "" && a.length < 50 ) {
+		console.log('done');
+		appData.expenses[a] = b;
+		i++;
+	} else {
+		console.log('bad result');
+		i--;
+	};
+} while ( i < 2 );*/
 
 appData.moneyPerDay = appData.budget / 30;
 
